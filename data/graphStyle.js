1b2663c732ea2817f250cff5446d1a25c417baf7
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2018-12-25 13:19:32 +0100

    Sizes in %

--- data/graphStyle.js
+++ data/graphStyle.js
@@ -3,8 +3,8 @@ graphStyle = [
     selector: 'node',
     style: {
         'label': 'data(label)',
-        'width': '200px',
-        'height': '200px',
+        'width': '100%',
+        'height': '100%',
         'color': 'black',
         'background-color': 'black',
         'font-size': 0
@@ -14,7 +14,7 @@ graphStyle = [
   {
     selector: 'edge',
     style: {
-        'width': '10px',
+        'width': '5%',
         'line-color': 'grey',
         'target-arrow-color': 'grey',
         'target-arrow-shape': 'triangle',
@@ -26,7 +26,7 @@ graphStyle = [
   {
     selector: '.childrenEdges',
     style: {
-        'width': '50px',
+        'width': '20%',
         'line-color': '#5a61c2',
         'target-arrow-color': '#5a61c2',
         'arrow-scale': 3
@@ -36,7 +36,7 @@ graphStyle = [
   {
     selector: '.parentEdges',
     style: {
-        'width': '50px',
+        'width': '20%',
         'line-color': '#ed5e9c',
         'target-arrow-color': '#ed5e9c',
         'arrow-scale': 3
@@ -48,12 +48,12 @@ graphStyle = [
     style: {
       'label': 'data(label)',
       'background-color': '#5a61c2',
-      'text-background-color': '#82ded7',
+      'text-background-color': '#ebebeb',
       'text-background-opacity': 0,
-      'width': '300px',
-      'height': '300px',
-      'font-size': 300,
-      'text-halign': 'left'
+      'width': '100%',
+      'height': '100%',
+      'font-size': 150,
+      'text-halign': 'center'
     }
   },
 
@@ -62,12 +62,12 @@ graphStyle = [
     style: {
       'label': 'data(label)',
       'background-color': '#ed5e9c',
-      'text-background-color': '#82ded7',
+      'text-background-color': '#ebebeb',
       'text-background-opacity': 0,
-      'width': '300px',
-      'height': '300px',
-      'font-size': 300,
-      'text-halign': 'left'
+      'width': '100%',
+      'height': '100%',
+      'font-size': 150,
+      'text-halign': 'center'
     }
   },
 
@@ -76,13 +76,24 @@ graphStyle = [
     style: {
       'label': 'data(label)',
       'background-color': '#17cfad',
-      'text-background-color': '#82ded7',
+      'text-background-color': '#ebebeb',
       'text-background-opacity': 0,
-      'width': '300px',
-      'height': '300px',
-      'font-size': 300,
-      'text-halign': 'left'
+      'z-index': 1,
+      'width': '100%',
+      'height': '100%',
+      'font-size': 150,
+      'text-halign': 'center'
     }
   }
+
+  // {
+  //   oldLabel = selectedNode.css('label');
+  //   oldColor = selectedNode.css('background-color');
+  //   let rxnName = selectedNode.data('rxnName');
+  //   selectedNode.css('label', rxnName);
+  //   selectedNode.css('text-background-opacity', 1);
+  //   selectedNode.css('background-color', '#17cfad');
+  //   selectedNode.css('text-halign', 'centre');
+  // }
 
 ]
